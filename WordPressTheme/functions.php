5146fd47c0df38260c69e8b705098a3389e0431a
mai-2 <71576918+mai-2@users.noreply.github.com>
2023-12-29 01:37:31 +0900

    Update functions.php

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -1,7 +1,16 @@
 <?php
+/**
+ * Functions
+ */
+
+/**
+ * CSSとJavaScriptの読み込み
+ *
+ * @codex https://wpdocs.osdn.jp/%E3%83%8A%E3%83%93%E3%82%B2%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%83%A1%E3%83%8B%E3%83%A5%E3%83%BC
+ */
 function my_script_init() {
-    wp_enqueue_style("my", get_template_directory_uri() . "assets/css/style.css", array(), filemtime(get_theme_file_path('css/style.css')), "all");
-    wp_enqueue_script("my", get_template_directory_uri() . "assets/js/script.js", array("jquery"), filemtime(get_theme_file_path('js/script.js')), true);
+	wp_enqueue_style( 'my', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime(get_theme_file_path('assets/css/style.css')), 'all' );
+	wp_enqueue_script( 'my', get_template_directory_uri() . '/assets/js/script.js', array( 'jquery' ), filemtime(get_theme_file_path('assets/js/script.js')), true );
 }
-add_action("wp_enqueue_scripts", "my_script_init");
+add_action('wp_enqueue_scripts', 'my_script_init');
 ?>
